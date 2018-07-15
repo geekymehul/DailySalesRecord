a93e17255f26674a8dccc7b92b1fe3c3b641044b
cheenu <gargcheenu57@gmail.com>
2018-07-15 20:12:58 +0530

    AddCustome

--- app/src/main/java/com/example/gargc/dailysalesrecord/Activity/AddCustomer.java
+++ app/src/main/java/com/example/gargc/dailysalesrecord/Activity/AddCustomer.java
@@ -1,14 +1,31 @@
 package com.example.gargc.dailysalesrecord.Activity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.gargc.dailysalesrecord.CustomerActivity;
 import com.example.gargc.dailysalesrecord.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AddCustomer extends AppCompatActivity {
 
@@ -17,7 +34,14 @@ public class AddCustomer extends AppCompatActivity {
     Button button;
     private ProgressBar progressBar;
 
+    Uri mImageUri=null;
+
     private String customerName,customerEmailId,customerAmountPaid,customerNotes,customerPhoneNumber;
+
+    CircleImageView circleImageView;
+
+    private DatabaseReference mDatabase;
+    StorageReference mStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +54,10 @@ public class AddCustomer extends AppCompatActivity {
         amountPaid=(EditText) findViewById(R.id.customerAmountPaid);
         notes=(EditText) findViewById(R.id.customerNotes);
         progressBar = (ProgressBar) findViewById(R.id.progress);
+        circleImageView=(CircleImageView) findViewById(R.id.addImage);
+
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("Customer");
+        mStorage = FirebaseStorage.getInstance().getReference();
 
         button=(Button) findViewById(R.id.btSignIn);
 
@@ -54,6 +82,11 @@ public class AddCustomer extends AppCompatActivity {
                     Toast.makeText(AddCustomer.this, "Please enter a valid mobile number", Toast.LENGTH_SHORT).show();
                     phoneNumber.requestFocus();
                 }
+                else if(mImageUri==null)
+                {
+                    Toast.makeText(AddCustomer.this, "Add Customer Image", Toast.LENGTH_SHORT).show();
+                    circleImageView.requestFocus();
+                }
                 else {
                     toggleViews(false);
                     button.setEnabled(false);
@@ -63,6 +96,15 @@ public class AddCustomer extends AppCompatActivity {
                 toggleViews(true);
                 button.setEnabled(true);
 
+            }
+        });
+
+        circleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent , 1);
 
             }
         });
@@ -71,6 +113,58 @@ public class AddCustomer extends AppCompatActivity {
     }
 
     private void addCustomer() {
+
+
+        StorageReference filepath = mStorage.child(mImageUri.getLastPathSegment());
+        Log.i("check","1");
+
+        filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Log.i("check",mImageUri+"2");
+
+
+                try {
+                    @SuppressWarnings("VisibleForTests")
+                    Uri imgUri = taskSnapshot.getDownloadUrl();
+
+                    DatabaseReference userReq = mDatabase.child(customerName);
+                    Log.i("check","3");
+
+
+
+                    HashMap userReqMap = new HashMap();
+                    userReqMap.put("CustomerName", customerName);
+                    userReqMap.put("EmailId", customerEmailId);
+                    userReqMap.put("PhoneNumber", customerPhoneNumber);
+                    userReqMap.put("AmountPaid", customerAmountPaid);
+                    userReqMap.put("Notes", customerNotes);
+                    userReqMap.put("Image", imgUri.toString());
+
+                    userReq.setValue(userReqMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(AddCustomer.this, "Customer Added successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(AddCustomer.this, CustomerActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(AddCustomer.this, "Please Try Again", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+                }
+                catch (Exception e)
+                {
+                    Log.i("error","true");
+                }
+            }
+        });
+
+
+
 
     }
 
@@ -84,5 +178,16 @@ public class AddCustomer extends AppCompatActivity {
         amountPaid.setEnabled(enabled);
         notes.setEnabled(enabled);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1 && resultCode==RESULT_OK)
+        {
+            mImageUri = data.getData();
+            circleImageView.setImageURI(mImageUri);
+        }
     }
 }
